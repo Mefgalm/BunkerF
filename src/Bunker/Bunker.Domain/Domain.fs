99bb1b040594c5b8a80ea0abf24eb46d763bd7a5
Vladyslav Kuznietsov <mefgalm@gmail.com>
2018-07-12 18:07:29 +0300

    add first api

--- src/Bunker/Bunker.Domain/Domain.fs
+++ src/Bunker/Bunker.Domain/Domain.fs
@@ -1,226 +1,257 @@
-module Domain 
-    open Microsoft.FSharp.Collections
-    open System
-    open System.Text.RegularExpressions
+module Domain
+
+open Microsoft.FSharp.Collections
+open System
+open System.Text.RegularExpressions
+
+type Result<'a> =
+    | Ok of 'a
+    | Fail of string list
+
+let bind f1 f2 =
+    match f2 with
+    | Ok x -> f1 x
+    | Fail error -> Fail error
+
+type ResultBuilder() =
     
-    type CompanyId =
+    member this.Bind(x, f) =
+        match x with
+        | Fail e -> Fail e
+        | Ok a -> f a
+    
+    member this.Return(x) = Ok x
+
+let result = new ResultBuilder()
+
+module Validation =
+    let regexCheck regex errorMessage value returnFun =
+        if not <| Regex.IsMatch(value, regex) then Fail [ errorMessage ]
+        else Ok <| returnFun value
+
+module CompanyId =
+    type T =
         | NewCompany
         | Id of int
-    
-    type CompanyName = CompanyName of string
-    
-    type CompanyDescription = CompanyDescription of string option
-    
-    type PlayerId =
-        | NewPlayer
-        | Id of int
-    
-    type PlayerNickName = PlayerNickName of string
-    
-    type PlayerFirstName = PlayerFirstName of string
-    
-    type ChallangeId =
-        | NewChallange
-        | Id of int
-    
-    type ChallangeName = ChallangeName of string
-    
-    type ChallangeDescription = ChallangeDescription of string option
-    
-    type TaskId =
-        | NewTask
-        | Id of int
-    
-    type TaskName = TaskName of string
-    
-    type TaskDescription = TaskDescription of string
-    
-    type TaskAnswer = TaskAnswer of string
-    
-    type TeamId =
-        | NewTeam
-        | Id of int
-    
-    type TeamName = TeamName of string
-    
-    type TeamDescription = TeamDescription of string option
-    
-    type AnswerId =
-        | NewAnswer
-        | Id of int
-    
-    type Player =
-        { Id : PlayerId
-          NickName : PlayerNickName
-          FirstName : PlayerFirstName
-          JoinedCompanies : Company list
-          JoinedTeams : Team list
-          OwnedCompanies : Company list
-          OwnedTeams : Team list }
-    
-    and Company =
-        { Id : CompanyId
-          Name : CompanyName
-          Owners : Player list
-          Challanges : Challange list
-          JoinedPlayers : Player list
-          Teams : Team list }
-    
-    and Challange =
-        { Id : ChallangeId
-          Name : ChallangeName
-          Description : ChallangeDescription
-          Tasks : Task list
-          Company : Company
-          JoinedTeams : Team list }
-    
-    and Task =
-        { Id : TaskId
-          Name : TaskName
-          Challange : Challange
-          Description : TaskDescription
-          Answer : TaskAnswer }
-    
-    and Team =
-        { Id : TeamId
-          Name : TeamName
-          Owner : Player
-          Description : TeamDescription
-          Company : Company
-          Players : Player list
-          Challanges : Challange list }
-    
-    and Answer =
-        { Id : AnswerId
-          Task : Task
-          Player : Player }
-    
-    type Result<'a> =
-        | Ok of 'a
-        | Fail of string list
-    
-    let bind f1 f2 =
-        match f2 with
-        | Ok x -> f1 x
-        | Fail error -> Fail error
-    
-    module Validation =
-        let regexCheck regex errorMessage value returnFun =
-            if not <| Regex.IsMatch(value, regex) then
-                Fail [errorMessage]
-            else Ok <| returnFun value
-    
-    module Player =        
         
-        let createNickName nickName =
-            Validation.regexCheck "^\w{1,20}$" "NickName is incorrect" nickName PlayerNickName
+    let value e =
+        function 
+        | NewCompany -> 0
+        | Id x -> x
+        
+module PlayerId =
+    type T =
+        | NewPlayer
+        | Id of int   
+    
+    let value e =
+        function 
+        | NewPlayer -> 0
+        | Id x -> x        
+
+module CompanyName =
+    type T = CompanyName of string
+    
+    let create name = Validation.regexCheck "^\w{4,16}$" "Comapny name like ^\w{4,16}$" name CompanyName
+    let value (CompanyName e) = e
+
+module CompanyDescription =
+    type T = CompanyDescription of string
+    
+    let create description = Validation.regexCheck "^\w{4,16}$" "Company Description like ^\w{4,16}$" description CompanyDescription
+    let value (CompanyDescription e) = e    
+
+module PlayerNickName =
+    type T = PlayerNickName of string
+    
+    let create name = Validation.regexCheck "^\w{4,16}$" "PlayerNickName like ^\w{4,16}$" name PlayerNickName
+    let value (PlayerNickName e) = e
+    
+module PlayerFirstName =
+    type T = PlayerFirstName of string
+    
+    let create name = Validation.regexCheck "^\w{4,16}$" "Name like ^\w{4,16}$" name PlayerFirstName
+    let value (PlayerFirstName e) = e    
+
+type ChallangeId =
+    | NewChallange
+    | Id of int
+
+type ChallangeName = ChallangeName of string
+
+type ChallangeDescription = ChallangeDescription of string option
+
+type TaskId =
+    | NewTask
+    | Id of int
+
+type TaskName = TaskName of string
+
+type TaskDescription = TaskDescription of string
+
+type TaskAnswer = TaskAnswer of string
+
+type TeamId =
+    | NewTeam
+    | Id of int
+
+type TeamName = TeamName of string
+
+type TeamDescription = TeamDescription of string option
+
+type AnswerId =
+    | NewAnswer
+    | Id of int
+
+type Player =
+    { Id : PlayerId.T
+      NickName : PlayerNickName.T
+      FirstName : PlayerFirstName.T
+      JoinedCompanies : Company list
+      JoinedTeams : Team list
+      OwnedCompanies : Company list
+      OwnedTeams : Team list }
+
+and Company =
+    { Id : CompanyId.T
+      Name : CompanyName.T
+      Owners : Player list
+      Challanges : Challange list
+      JoinedPlayers : Player list
+      Teams : Team list }
+
+and Challange =
+    { Id : ChallangeId
+      Name : ChallangeName
+      Description : ChallangeDescription
+      Tasks : Task list
+      Company : Company
+      JoinedTeams : Team list }
+
+and Task =
+    { Id : TaskId
+      Name : TaskName
+      Challange : Challange
+      Description : TaskDescription
+      Answer : TaskAnswer }
+
+and Team =
+    { Id : TeamId
+      Name : TeamName
+      Owner : Player
+      Description : TeamDescription
+      Company : Company
+      Players : Player list
+      Challanges : Challange list }
+
+and Answer =
+    { Id : AnswerId
+      Task : Task
+      Player : Player }
+
+module Player = 
+    let create nickName firstName =
+        result { 
+            let! nickName = PlayerNickName.create nickName
+            let! firstName = PlayerFirstName.create firstName
             
-        let createFirstName firstName =
-            Validation.regexCheck "^\w{1,20}$" "FirstName is incorrect" firstName PlayerFirstName
-                
-        let create nickName firstName =        
-            match createNickName nickName, createFirstName firstName with
-            | Ok n, Ok f ->         
-                Ok { Id = NewPlayer
-                     NickName = n
-                     FirstName = f
+            return { Id = PlayerId.NewPlayer
+                     NickName = nickName
+                     FirstName = firstName
                      JoinedCompanies = []
                      JoinedTeams = []
                      OwnedCompanies = []
                      OwnedTeams = [] }
-            | Fail nl, Fail fl -> Fail <| nl @ fl 
-            | Fail nl, _ -> Fail nl
-            | _, Fail fl -> Fail fl                                            
-            
-        
-        let update player nickName firstName =
-            { player with NickName = nickName
-                          FirstName = firstName }
+        }
     
-    module Company =
-        let findCompany companyId player =
-            match player.OwnedCompanies |> List.tryFind (fun x -> x.Id = companyId) with
-            | Some company -> Ok company
-            | None -> Fail [ "Company not found" ]
-        
-        let create companyName player =
-            { Id = NewCompany
-              Name = companyName
-              Owners = [ player ]
-              Challanges = []
-              JoinedPlayers = []
-              Teams = [] }
-        
-        let update player companyId companyName =
-            companyId |> (findCompany player >> bind (fun company -> Ok { company with Name = companyName }))
-        
-        let addOwner owner companyId player =
-            match owner |> findCompany companyId with
-            | Ok company -> 
-                if company.Owners |> List.exists ((=) player) then Fail [ "Player already added" ]
-                else Ok { company with Owners = player :: company.Owners }
-            | Fail errors -> Fail errors
+    let update player nickName firstName =
+        { player with NickName = nickName
+                      FirstName = firstName }
+
+module Company =
+    let findCompany companyId player =
+        match player.OwnedCompanies |> List.tryFind (fun x -> x.Id = companyId) with
+        | Some company -> Ok company
+        | None -> Fail [ "Company not found" ]
     
-    module Team =
-        let findTeam player teamId =
-            match player.OwnedTeams |> List.tryFind (fun x -> x.Id = teamId) with
-            | Some company -> Ok company
-            | None -> Fail [ "Team not found" ]
-        
-        let create player company teamName teamDescription =
-            match player.OwnedTeams |> List.exists (fun x -> x.Company = company) with
-            | true -> Fail [ "Player can have only one team in company" ]
-            | false -> 
-                Ok { Id = NewTeam
-                     Name = teamName
-                     Owner = player
-                     Description = teamDescription
-                     Company = company
-                     Players = []
-                     Challanges = [] }
-        
-        let update player teamId teamName teamDescription =
-            match player |> findTeam teamId with
-            | Ok team -> 
-                Ok { team with Name = teamName
-                               Description = teamDescription }
-            | Fail errors -> Fail errors
+    let create companyName player =
+        { Id = CompanyId.NewCompany
+          Name = companyName
+          Owners = [ player ]
+          Challanges = []
+          JoinedPlayers = []
+          Teams = [] }
     
-    module Challange =
-        let create player companyId challangeName challangeDescription =
-            match player |> (Company.findCompany companyId) with
-            | Ok company -> 
-                Ok { Id = NewChallange
-                     Name = challangeName
-                     Description = challangeDescription
-                     Tasks = []
-                     Company = company
-                     JoinedTeams = [] }
-            | Fail errors -> Fail errors
+    let update player companyId companyName =
+        companyId |> (findCompany player >> bind (fun company -> Ok { company with Name = companyName }))
     
-    module Task =
-        let create player challangeId taskName taskDescription taskAnswer =
-            match player.OwnedCompanies
-                  |> List.collect (fun x -> x.Challanges)
-                  |> List.tryFind (fun x -> x.Id = challangeId) with
-            | Some challange -> 
-                Ok { Id = NewTask
-                     Name = taskName
-                     Challange = challange
-                     Description = taskDescription
-                     Answer = taskAnswer }
-            | None -> Fail [ "Challange not found" ]
+    let addOwner owner companyId player =
+        match owner |> findCompany companyId with
+        | Ok company -> 
+            if company.Owners |> List.exists ((=) player) then Fail [ "Player already added" ]
+            else Ok { company with Owners = player :: company.Owners }
+        | Fail errors -> Fail errors
+
+module Team =
+    let findTeam player teamId =
+        match player.OwnedTeams |> List.tryFind (fun x -> x.Id = teamId) with
+        | Some company -> Ok company
+        | None -> Fail [ "Team not found" ]
     
-    module Answer =
-        let create (player : Player) task answer =
-            match player.JoinedTeams
-                  |> List.collect (fun x -> x.Challanges)
-                  |> List.collect (fun x -> x.Tasks)
-                  |> List.tryFind ((=) task) with
-            | Some task -> 
-                if task.Answer = answer then 
-                    Ok { Id = NewAnswer
-                         Task = task
-                         Player = player }
-                else Fail [ "Answer is wrong" ]
-            | None -> Fail [ "Task not found" ]
+    let create player company teamName teamDescription =
+        match player.OwnedTeams |> List.exists (fun x -> x.Company = company) with
+        | true -> Fail [ "Player can have only one team in company" ]
+        | false -> 
+            Ok { Id = NewTeam
+                 Name = teamName
+                 Owner = player
+                 Description = teamDescription
+                 Company = company
+                 Players = []
+                 Challanges = [] }
+    
+    let update player teamId teamName teamDescription =
+        match player |> findTeam teamId with
+        | Ok team -> 
+            Ok { team with Name = teamName
+                           Description = teamDescription }
+        | Fail errors -> Fail errors
+
+module Challange =
+    let create player companyId challangeName challangeDescription =
+        match player |> (Company.findCompany companyId) with
+        | Ok company -> 
+            Ok { Id = NewChallange
+                 Name = challangeName
+                 Description = challangeDescription
+                 Tasks = []
+                 Company = company
+                 JoinedTeams = [] }
+        | Fail errors -> Fail errors
+
+module Task =
+    let create player challangeId taskName taskDescription taskAnswer =
+        match player.OwnedCompanies
+              |> List.collect (fun x -> x.Challanges)
+              |> List.tryFind (fun x -> x.Id = challangeId) with
+        | Some challange -> 
+            Ok { Id = NewTask
+                 Name = taskName
+                 Challange = challange
+                 Description = taskDescription
+                 Answer = taskAnswer }
+        | None -> Fail [ "Challange not found" ]
+
+module Answer =
+    let create (player : Player) task answer =
+        match player.JoinedTeams
+              |> List.collect (fun x -> x.Challanges)
+              |> List.collect (fun x -> x.Tasks)
+              |> List.tryFind ((=) task) with
+        | Some task -> 
+            if task.Answer = answer then 
+                Ok { Id = NewAnswer
+                     Task = task
+                     Player = player }
+            else Fail [ "Answer is wrong" ]
+        | None -> Fail [ "Task not found" ]
--- src/Bunker/Program.fs
+++ src/Bunker/Program.fs
@@ -1,105 +1,102 @@
 module BrainBunker.App
 
-open System
-open System.IO
+open Bunker.Database
+open Domain
+open Giraffe
+open Microsoft.AspNetCore
 open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Cors.Infrastructure
 open Microsoft.AspNetCore.Hosting
-open Microsoft.Extensions.Logging
-open Microsoft.AspNetCore
-open Microsoft.AspNetCore.Hosting
-open Microsoft.Extensions.DependencyInjection
-open Giraffe
 open Microsoft.EntityFrameworkCore
-open Bunker.Database
-open Domain
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Logging
+open System
+open System.IO
 
 // ---------------------------------
 // Models
 // ---------------------------------
+module DomainEntity =
+    let mapPlayer player =
+        Bunker.Database.Entities.Player(
+            NickName = PlayerNickName.value player.NickName,
+            FirstName = PlayerFirstName.value player.FirstName,
+            LastName = "",
+            Email = "mefgalm@gmail.com",
+            PasswordHash = [|(byte) 1|],
+            PasswordSalt = [|(byte) 3|]            
+        )               
 
 type Message =
-    {
-        Text : string
-    }
+    { Text : string }
 
 // ---------------------------------
 // Views
 // ---------------------------------
 
+module Repository =
+    
+    let saveToDb<'a when 'a : not struct> (dbContext: BunkerDbContext) mapper domainObject =                 
+        match domainObject with
+        | Ok domObj -> 
+            try 
+                dbContext.Set<'a>().Add(domObj |> mapper) |> ignore
+                dbContext.SaveChanges() |> ignore
+                Ok domObj
+            with e -> Fail [e.Message] 
+        | Fail e -> Fail e                
+
 module Views =
     open GiraffeViewEngine
-
-    let layout (content: XmlNode list) =
-        html [] [
-            head [] [
-                title []  [ encodedText "BrainBunker" ]
-                link [ _rel  "stylesheet"
-                       _type "text/css"
-                       _href "/main.css" ]
-            ]
-            body [] content
-        ]
-
-    let partial () =
-        h1 [] [ encodedText "BrainBunker" ]
-
+    
+    let layout (content : XmlNode list) =
+        html [] [ head [] [ title [] [ encodedText "BrainBunker" ]
+                            link [ _rel "stylesheet"
+                                   _type "text/css"
+                                   _href "/main.css" ] ]
+                  body [] content ]
+    
+    let partial() = h1 [] [ encodedText "BrainBunker" ]
+    
     let index (model : Message) =
-        [
-            partial()
-            p [] [ encodedText model.Text ]
-        ] |> layout
+        [ partial()
+          p [] [ encodedText model.Text ] ]
+        |> layout
 
 // ---------------------------------
 // Web app
 // ---------------------------------
-
 let indexHandler (name : string) =
     let greetings = sprintf "Hello %s, from Giraffe!" name
-    let model     = { Text = greetings }
-    let view      = Views.index model
+    let model = { Text = greetings }
+    let view = Views.index model
     htmlView view
 
 [<CLIMutable>]
-type LoginModel =
-    {
-        NickName : string
-        Name : string
-    }
+type PlayerCreateRequest =
+    { NickName : string
+      Name : string }
 
-let loginHandler : HttpHandler =
-    fun next ctx ->
-        task {
-            let! model = ctx.BindFormAsync<LoginModel>()
+let createUser : HttpHandler =
+    fun next ctx -> 
+        task { 
+            let! model = ctx.BindJsonAsync<PlayerCreateRequest>()            
+            let playerResult = Player.create model.NickName model.Name            
+            let dbContext = ctx.GetService<BunkerDbContext>()            
+            let saveToDb = Repository.saveToDb dbContext DomainEntity.mapPlayer playerResult
             
-            let test = Player.create "" ""
-            
-            return! text "g" next ctx
-            //let dbContext = ctx.GetService<BunkerDbContext>()
-            
-                                    
-//            match result.Succeeded wit
-//            | true  -> return! redirectTo false "/user" next ctx
-//            | false -> return! htmlView (loginPage true) next ctx
+            return! json saveToDb next ctx
         }
 
 let webApp =
-    choose [
-        GET >=>
-            choose [
-                route "/" >=> indexHandler "world"
-                routef "/hello/%s" indexHandler
-            ]
-        POST >=>
-            choose [
-                route "/user"
-            ]
-        setStatusCode 404 >=> text "Not Found" ]
+    choose [ GET >=> choose [ route "/" >=> indexHandler "world"
+                              routef "/hello/%s" indexHandler ]
+             POST >=> choose [ route "/user" >=> createUser ]
+             setStatusCode 404 >=> text "Not Found" ]
 
 // ---------------------------------
 // Error handler
 // ---------------------------------
-
 let errorHandler (ex : Exception) (logger : ILogger) =
     logger.LogError(EventId(), ex, "An unhandled exception has occurred while executing the request.")
     clearResponse >=> setStatusCode 500 >=> text ex.Message
@@ -107,26 +104,19 @@ let errorHandler (ex : Exception) (logger : ILogger) =
 // ---------------------------------
 // Config and Main
 // ---------------------------------
-
 let configureCors (builder : CorsPolicyBuilder) =
-    builder.WithOrigins("http://localhost:8080")
-           .AllowAnyMethod()
-           .AllowAnyHeader()
-           |> ignore
+    builder.WithOrigins("http://localhost:8080").AllowAnyMethod().AllowAnyHeader() |> ignore
 
 let configureApp (app : IApplicationBuilder) =
     let env = app.ApplicationServices.GetService<IHostingEnvironment>()
     (match env.IsDevelopment() with
-    | true  -> app.UseDeveloperExceptionPage()
-    | false -> app.UseGiraffeErrorHandler errorHandler)
-        .UseCors(configureCors)
-        .UseStaticFiles()
-        .UseGiraffe(webApp)
+     | true -> app.UseDeveloperExceptionPage()
+     | false -> app.UseGiraffeErrorHandler errorHandler).UseCors(configureCors).UseStaticFiles().UseGiraffe(webApp)
 
 let configureServices (services : IServiceCollection) =
-    services.AddEntityFrameworkNpgsql()
-            .AddDbContext<BunkerDbContext>(fun o -> o.UseNpgsql("User ID=postgres;Password=1Q2w3e4r;Host=localhost;Database=Bunker;") |> ignore ) |> ignore
-    services.AddCors()    |> ignore
+    services.AddEntityFrameworkNpgsql().AddDbContext<BunkerDbContext> 
+        (fun o -> o.UseNpgsql("User ID=postgres;Password=1Q2w3e4r;Host=localhost;Database=Bunker;") |> ignore) |> ignore
+    services.AddCors() |> ignore
     services.AddGiraffe() |> ignore
 
 let configureLogging (builder : ILoggingBuilder) =
@@ -135,20 +125,12 @@ let configureLogging (builder : ILoggingBuilder) =
 
 let CreateWebHostBuilder args =
     let contentRoot = Directory.GetCurrentDirectory()
-    let webRoot     = Path.Combine(contentRoot, "WebRoot")
+    let webRoot = Path.Combine(contentRoot, "WebRoot")
+    WebHost.CreateDefaultBuilder(args).UseKestrel().UseContentRoot(contentRoot).UseIISIntegration().UseWebRoot(webRoot)
+           .Configure(Action<IApplicationBuilder> configureApp).ConfigureServices(configureServices)
+           .ConfigureLogging(configureLogging)
 
-    WebHost
-        .CreateDefaultBuilder(args)
-        .UseKestrel()
-        .UseContentRoot(contentRoot)
-        .UseIISIntegration()
-        .UseWebRoot(webRoot)
-        .Configure(Action<IApplicationBuilder> configureApp)
-        .ConfigureServices(configureServices)
-        .ConfigureLogging(configureLogging)
-        
 [<EntryPoint>]
 let main args =
     CreateWebHostBuilder(args).Build().Run()
-    
     0
